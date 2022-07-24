78a3c595f1e0706aecd74048c3caedb8a4f02a7e
seraphim <serafimcomru91@gmail.com>
2022-07-24 20:24:50 +0300

    afd

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -1,11 +1,6 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import { test, expect } from '@jest/globals';
 import getDiff from '../index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 test('half', () => {
   expect(4).toBe(4);
 });
