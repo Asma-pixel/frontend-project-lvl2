e681b0a2fe48edf873a463186762f67d44f7150e
seraphim <serafimcomru91@gmail.com>
2022-07-25 19:50:05 +0300

    adf

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -1,6 +1,16 @@
 import { test, expect } from '@jest/globals';
 import getDiff from '../index.js'
-
-test('test', () => {
-  expect(4).toBe(4);
+const str = 
+`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true 
+}`
+test('getDiff', () => {
+  const testStr = getDiff('./files/file1.json', './files/file1.json');
+  console.log(testStr);
+  expect(testStr).not.toEqual(str);
 });
