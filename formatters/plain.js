00144c1db84db6c1e890d8cf42d48161e58211b5
seraphim <serafimcomru91@gmail.com>
2022-08-05 20:19:36 +0300

    8th step

--- formatters/plain.js
+++ formatters/plain.js
@@ -10,12 +10,12 @@ const plain = (tree, path = []) => {
     const currentPath = [path, node.name].flat();
     switch (node.type) {
       case 'withChildrens': return plain(node.children, currentPath);
-      case 'equalValue': return `Property '${currentPath.join('.')}' was added with value: ${getValue(node.property)}`;
+      case 'equalValue': return;
       case 'hasOnlyFirstProp': return `Property '${currentPath.join('.')}' was removed`;
       case 'hasOnlySecProp': return `Property '${currentPath.join('.')}' was added with value: ${getValue(node.property)}`;
       default: return `Property '${currentPath.join('.')}' was updated. From ${getValue(node.firstProperty)} to ${getValue(node.secondProperty)}`;
     }
   });
-  return formatedData.join('\n');
+  return formatedData.filter(n=>n).join('\n');
 };
 export default plain;
